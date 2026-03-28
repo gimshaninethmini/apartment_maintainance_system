dead79dfe982a3fe739b0f3228e46dedef41861d
Gimshani Nethmini <gimshanin@gmail.com>
2026-03-28 12:39:07 +0530

    Registering models in admin panel

--- maintenance/admin.py
+++ maintenance/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import UserProfile, MaintenanceRequest, Assignment, UpdateLog
+
+admin.site.register(UserProfile)
+admin.site.register(MaintenanceRequest)
+admin.site.register(Assignment)
+admin.site.register(UpdateLog)
