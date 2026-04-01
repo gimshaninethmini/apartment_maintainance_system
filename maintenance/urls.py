06be809cdd8c8b75d06952ceb68ac6615986fab3
Dilki__MMM <y.dil2003@gmail.com>
2026-04-01 13:19:44 +0530

    Added URL for exporting requests to CSV

--- maintenance/urls.py
+++ maintenance/urls.py
@@ -1,2 +1,3 @@
 path('manager/request/<int:request_id>/', views.manager_request_detail_view, name='manager_request_detail'),
 path('manager/update/<int:request_id>/', views.manager_update_status_view, name='manager_update_status'),
+path('export/requests/', views.export_requests_csv, name='export_requests'),
