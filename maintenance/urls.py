d6edf0c31ddf08b535867e8ad92328dedec113a8
Dilki__MMM <y.dil2003@gmail.com>
2026-04-01 12:45:38 +0530

    Added manager request detail and status update URLs

---
+++ maintenance/urls.py
@@ -0,0 +1,2 @@
+path('manager/request/<int:request_id>/', views.manager_request_detail_view, name='manager_request_detail'),
+path('manager/update/<int:request_id>/', views.manager_update_status_view, name='manager_update_status'),
