217b6eb1d82d4b08a301400a0577d6401fed4cb8
Dilki__MMM <y.dil2003@gmail.com>
2026-04-24 01:57:54 +0530

    Changed the appearence of brand icon

--- maintenance/views.py
+++ maintenance/views.py
@@ -86,6 +86,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, f'Welcome back, {username}! You have successfully logged in.')
             return redirect('dashboard')
         else:
             messages.error(request, 'Invalid credentials')
